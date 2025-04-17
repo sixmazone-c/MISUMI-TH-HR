9b106876ed3ecfae1e20e7ce4128ee0694ee28f0
[M-Tha] Suriya Chieblaem <it.Tkf3.04@misumi.co.th>
2025-04-17 16:41:37 +0700

    final

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
+import { Fragment } from "react";
+import Head from "next/head";
 
-import FeaturedPosts from '../components/home-page/featured-posts';
-import Hero from '../components/home-page/hero';
-import { getFeaturedPosts } from '../lib/posts-util';
+import FeaturedPosts from "../components/home-page/featured-posts";
+import Hero from "../components/home-page/hero";
+import { getFeaturedPosts } from "../lib/posts-util";
 
 function HomePage(props) {
   return (
@@ -11,8 +11,8 @@ function HomePage(props) {
       <Head>
         <title>Max' Blog</title>
         <meta
-          name='description'
-          content='I post about programming and web development.'
+          name="description"
+          content="I post about programming and web development. aa"
         />
       </Head>
       <Hero />
